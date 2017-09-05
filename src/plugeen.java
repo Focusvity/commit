0296c65df0313f23aae68272401f40623fd592df
Ronit Sajeey <rsajeey@hotmail.com>
2017-09-05 17:39:02 +0100

    Update plugeen.java

--- src/plugeen.java
+++ src/plugeen.java
@@ -3,11 +3,11 @@ public class plugeen extends Plugin // Pls
 {
     public static void onEnable()
     {
-    log.info("[Kindergarten] wille is a buttwad");
+      log.info("[Kindergarten] wille is a buttwad");
     }
     public static void onDisable()
     {
-    log.info("[Kindergarten] clean up, clean up, everybody clean up");
+      log.info("[Kindergarten] clean up, clean up, everybody clean up");
     }
 }
 BREAK BREAK // Pls
@@ -15,26 +15,23 @@ BREAK // Pls
 
 if (this.isBroken())
 { 
-bcastMsg("PIEGUY BROKE IT!"); 
-} 
-else 
-{ 
-bcastMsg("PIEGUY BROKE IT!"); 
+  bcastMsg("PIEGUY BROKE IT!"); 
+} else { 
+  bcastMsg("PIEGUY BROKE IT!"); 
 }
 
 if (Robin.isPoo()) 
 { 
-bcastMsg("BAD ROBINPOO"); 
-} 
-else 
-{ 
-bcastMsg("BAD ROBINPOO"); 
+  bcastMsg("BAD ROBINPOO"); 
+} else { 
+  bcastMsg("BAD ROBINPOO"); 
 }
+
 Wilee wilee = new Wilee:
 Buttwad buttwad = new Buttwad;
-if (Wilee.isButtwad){
-TFM_Util.bcastMsg(ChatColor.RED + "BUTTWAD ALERT, BUTTWAD ALERT, CLASS!!");
-}
-else{
-TFM_Util.bcastMsg(ChatColor.GREEN + "All clear. c:");
+
+if (Wilee.isButtwad) {
+  TFM_Util.bcastMsg(ChatColor.RED + "BUTTWAD ALERT, BUTTWAD ALERT, CLASS!!");
+} else {
+  TFM_Util.bcastMsg(ChatColor.GREEN + "All clear. c:");
 }
